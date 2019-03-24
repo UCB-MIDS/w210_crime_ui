f6bfb30047d4e6198a095ed9b675b27c211adf1f
Felipe Cruz Neiva Campos <felipe.pqi@gmail.com>
2019-03-24 16:40:38 -0300

    Modified application UI template to work with Chicago crime predictions

--- static/js/pages/dashboard.js
+++ static/js/pages/dashboard.js
@@ -9,6 +9,60 @@ $(function () {
 
   "use strict";
 
+  function update_graphs(communityArea,weekDay,weekYear,hourDay)
+  {
+    var params=undefined;
+    if (typeof communityArea !== 'undefined') {
+      params={communityarea: JSON.stringify(communityArea), weekday: JSON.stringify(weekDay), weekyear: JSON.stringify(weekYear), hourday: JSON.stringify(hourDay)};
+    }
+    else {
+      params={weekday: JSON.stringify(weekDay), weekyear: JSON.stringify(weekYear), hourday: JSON.stringify(hourDay)};
+    }
+    var results=undefined;
+    console.log(params)
+    $.ajax({
+            url: "http://localhost:60000/predict",
+            data: params,
+            type: "POST",
+            success: function (data) {
+              var mapData = {};
+              var crimeTypeData = {};
+              var preds = data.result;
+
+              preds.forEach(function(item) {
+                  if (typeof mapData[item["communityArea"]] == 'undefined') {
+                    mapData[item["communityArea"]] = 0
+                  }
+                  if (typeof crimeTypeData[item["primaryType"]] == 'undefined') {
+                    crimeTypeData[item["primaryType"]] = 0
+                  }
+                  mapData[item["communityArea"]] = mapData[item["communityArea"]] + item["pred"]
+                  crimeTypeData[item["primaryType"]] = crimeTypeData[item["primaryType"]] + item["pred"]
+              })
+
+              var mapObject = $('#world-map').vectorMap('get', 'mapObject');
+              if (typeof mapObject != 'undefined') {
+                mapObject.remove();
+              }
+              //mapObject.series.regions[0].setValues(mapData);
+              draw_map(mapData);
+
+              var chartData = []
+              Object.keys(crimeTypeData).forEach(function(key) {
+                chartData.push({'label': key, 'value': crimeTypeData[key]});
+              })
+              area.setData(chartData);
+              donut.setData(chartData);
+              area.redraw();
+              donut.redraw();
+
+            },
+            error: function (x, y, z) {
+               console.log(x);
+            }
+        });
+  }
+
   //Make the dashboard widgets sortable Using jquery UI
   $(".connectedSortable").sortable({
     placeholder: "sort-highlight",
@@ -32,61 +86,83 @@ $(function () {
 
   $('.daterange').daterangepicker({
     ranges: {
-      'Today': [moment(), moment()],
-      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-      'This Month': [moment().startOf('month'), moment().endOf('month')],
-      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+      'Now': [moment(),moment()],
+      'In 6 hours': [moment().add(6, 'hours'),moment().add(6, 'hours')],
+      'In 12 hours': [moment().add(12, 'hours'),moment().add(12, 'hours')],
+      'In 18 hours': [moment().add(18, 'hours'),moment().add(18, 'hours')],
+      'Tomorrow': [moment().add(1, 'days'),moment().add(1, 'days')]
     },
-    startDate: moment().subtract(29, 'days'),
-    endDate: moment()
-  }, function (start, end) {
-    window.alert("You chose: " + start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+    locale: {
+        format: 'MM/DD/YYYY HH:mm:ss',
+        separator: ' - ',
+        applyLabel: 'Apply',
+        cancelLabel: 'Cancel',
+        weekLabel: 'W',
+        customRangeLabel: 'Custom Range',
+        daysOfWeek: moment.weekdaysMin(),
+        monthNames: moment.monthsShort(),
+        firstDay: moment.localeData().firstDayOfWeek()
+    },
+    startDate: moment(),
+    endDate: moment(),
+    timePicker: true,
+    singleDatePicker: true,
+    timePicker24Hour: true
+  }, function (start,end) {
+    /* console.log("You chose: " + start.format('MMMM Do YYYY, HH:mm:ss') + " - " + end.format('MMMM Do YYYY, HH:mm:ss')); */
+    var weekDay = start.day();
+    var weekYear = start.week();
+    var hourDay = undefined;
+    if (start.hour() >= 0 && start.hour() <= 6) {
+      hourDay = 'DAWN';
+    } else if (start.hour() > 6 && start.hour() <= 11) {
+      hourDay = 'MORNING';
+    } else if (start.hour() > 11 && start.hour() <= 17) {
+      hourDay = 'AFTERNOON';
+    } else {
+      hourDay = 'EVENING';
+    }
+    /*
+    console.log("Day of the Week: " + weekDay);
+    console.log("Week of Year: " + weekYear);
+    console.log("Time of the Day: " + hourDay);
+    */
+    update_graphs(undefined, [weekDay], [weekYear], [hourDay]);
   });
 
   /* jQueryKnob */
   $(".knob").knob();
 
-  //jvectormap data
-  var visitorsData = {
-    "US": 398, //USA
-    "SA": 400, //Saudi Arabia
-    "CA": 1000, //Canada
-    "DE": 500, //Germany
-    "FR": 760, //France
-    "CN": 300, //China
-    "AU": 700, //Australia
-    "BR": 600, //Brazil
-    "IN": 800, //India
-    "GB": 320, //Great Britain
-    "RU": 3000 //Russia
-  };
-  //World map by jvectormap
-  $('#world-map').vectorMap({
-    map: 'world_mill_en',
-    backgroundColor: "transparent",
-    regionStyle: {
-      initial: {
-        fill: '#e4e4e4',
-        "fill-opacity": 1,
-        stroke: 'none',
-        "stroke-width": 0,
-        "stroke-opacity": 1
+  function draw_map(data) {
+    //jvectormap data
+    var crimeData = data;
+    //World map by jvectormap
+    $('#world-map').vectorMap({
+      map: 'us-il-chicago_mill',
+      backgroundColor: "transparent",
+      regionStyle: {
+        initial: {
+          fill: '#e4e4e4',
+          "fill-opacity": 1,
+          stroke: 'none',
+          "stroke-width": 0,
+          "stroke-opacity": 1
+        }
+      },
+      series: {
+        regions: [{
+          values: crimeData,
+          scale: ["#FFCCCC", "#FF0000"],
+          normalizeFunction: 'polynomial'
+        }]
+      },
+      onRegionLabelShow: function (e, el, code) {
+        if (typeof crimeData[code] != "undefined")
+          el.html(el.html() + ': ' + crimeData[code] + ' total crimes');
       }
-    },
-    series: {
-      regions: [{
-        values: visitorsData,
-        scale: ["#92c1dc", "#ebf4f9"],
-        normalizeFunction: 'polynomial'
-      }]
-    },
-    onRegionLabelShow: function (e, el, code) {
-      if (typeof visitorsData[code] != "undefined")
-        el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
-    }
-  });
+    });
+  }
+  draw_map({})
 
   //Sparkline charts
   var myvalues = [1000, 1200, 920, 927, 931, 1027, 819, 930, 1021];
@@ -124,25 +200,15 @@ $(function () {
 
   /* Morris.js Charts */
   // Sales chart
-  var area = new Morris.Area({
+  var area = new Morris.Bar({
     element: 'revenue-chart',
     resize: true,
     data: [
-      {y: '2011 Q1', item1: 2666, item2: 2666},
-      {y: '2011 Q2', item1: 2778, item2: 2294},
-      {y: '2011 Q3', item1: 4912, item2: 1969},
-      {y: '2011 Q4', item1: 3767, item2: 3597},
-      {y: '2012 Q1', item1: 6810, item2: 1914},
-      {y: '2012 Q2', item1: 5670, item2: 4293},
-      {y: '2012 Q3', item1: 4820, item2: 3795},
-      {y: '2012 Q4', item1: 15073, item2: 5967},
-      {y: '2013 Q1', item1: 10687, item2: 4460},
-      {y: '2013 Q2', item1: 8432, item2: 5713}
     ],
-    xkey: 'y',
-    ykeys: ['item1', 'item2'],
-    labels: ['Item 1', 'Item 2'],
-    lineColors: ['#a0d0e0', '#3c8dbc'],
+    xkey: 'label',
+    ykeys: ['value'],
+    labels: ['Predicted'],
+    lineColors: ['#a0d0e0'],
     hideHover: 'auto'
   });
   var line = new Morris.Line({
@@ -206,5 +272,19 @@ $(function () {
       return ele;
     }
   });
+
+  var weekDay = moment().day();
+  var weekYear = moment().week();
+  var hourDay = undefined;
+  if (moment().hour() >= 0 && moment().hour() <= 6) {
+    hourDay = 'DAWN';
+  } else if (moment().hour() > 6 && moment().hour() <= 11) {
+    hourDay = 'MORNING';
+  } else if (moment().hour() > 11 && moment().hour() <= 17) {
+    hourDay = 'AFTERNOON';
+  } else {
+    hourDay = 'EVENING';
+  }
+  update_graphs(undefined, [weekDay], [weekYear], [hourDay])
 
 });
